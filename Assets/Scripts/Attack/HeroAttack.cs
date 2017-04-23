c24357cc3fb55449a88d4e4f3440ea6bce549ddd
Carlos Congosto <kko-tb_92@hotmail.com>
2017-04-23 11:15:29 +0200

    Movimientos y ataques personajes

---
+++ Assets/Scripts/Attack/HeroAttack.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroAttack : MonoBehaviour {
+
+    [SerializeField]
+    GameObject campoAtaque;
+    BoxCollider2D triggerAttack;
+
+    //Transform Enemy;
+    EnemyAttack eAtack;
+    int danioHero = 10;
+    LifeEnemy lifeE;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        lifeE = new LifeEnemy();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("enemigoSphere"))
+        {
+            Debug.Log("puedo atacar");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("enemigoSphere"))
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                lifeE.life -= danioHero;
+                Debug.Log("He hecho daño. Ahora tiene "+ lifeE.life);
+
+            }
+
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Movements/HeroMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+    public float speed = 10.0F; //Velocidad de movimiento
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal")*speed*Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
+    }
+
+    
+
+    
+}
