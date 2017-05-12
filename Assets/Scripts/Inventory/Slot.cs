18c8230eb0d4a5dd93680334b3ccf3381b7b1da6
gorco <antcal01@ucm.es>
2017-05-12 09:34:23 +0200

    Intventory first version

---
+++ Assets/Scripts/Inventory/Slot.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class Slot : MonoBehaviour, IPointerClickHandler {
+
+	public Image img;
+	public Item item;
+
+	private bool specialized;
+	private ItemType spezialitation;
+
+	void Start()
+	{
+		this.img = this.transform.GetChild(0).GetComponentInChildren<Image>();
+	}
+
+	public void AddSpecialization(ItemType spezialitation)
+	{
+		this.spezialitation = spezialitation;
+		this.specialized = true;
+	}
+
+	public ItemType GetSpecialization()
+	{
+		return this.spezialitation;
+	}
+
+	public bool IsSpecialized()
+	{
+		return this.specialized;
+	}
+
+	public void AddItem(Item item)
+	{
+		ChangeItem(item);
+	}
+
+	public Item GetCurrentItem()
+	{
+		return this.item;
+	}
+
+	public bool ChangeItem(Item newItem)
+	{
+		this.item = newItem;
+		if (item != null)
+		{
+			if (!specialized || spezialitation == item.itemType)
+			{
+				if (this.img == null)
+				{
+					this.img = this.transform.GetChild(0).GetComponent<Image>();
+				}
+				this.img.enabled = true;
+				this.img.sprite = item.itemSprite;
+				return true;
+			}
+		}
+		else
+		{
+			this.img.sprite = null;
+			this.img.enabled = false;
+		}
+
+		return false;
+
+	}
+
+	public void HideItem(Boolean hide)
+	{
+		this.img.enabled = !hide;
+	}
+
+	public void RemoveItem()
+	{
+		this.item = null;
+		this.img.enabled = false;
+	}
+
+	public bool IsEmpty()
+	{
+		return this.item == null;
+	}
+
+	public void UseItem()
+	{
+		if (!IsEmpty())
+		{
+			if (item.CanBeEquiped())
+			{
+				Inventory.Inv.EquipItem(this);
+			} 
+			else if (item.CanBeConsumed())
+			{
+				item.Use();
+				Inventory.Inv.IncreaseEmptyCount();
+				item = null;
+				img.enabled = false;
+			}
+			else if (item.CanBeUsed())
+			{
+
+			}
+		}
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if(eventData.button == PointerEventData.InputButton.Right)
+		{
+			UseItem();
+		}
+	}
+}
---
+++ Assets/Scripts/TakeItem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	//OnTriggerEnter2D is sent when another object enters a trigger collider attached to this object (2D physics only).
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Item")
+		{
+			if (Inventory.Inv.AddItem(other.GetComponent<Item>()))
+			{
+				other.gameObject.SetActive(false);
+			}
+		}
+	}
+}
